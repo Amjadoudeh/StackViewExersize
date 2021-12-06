6ed34d8fbd465c3a643854e6ea6234b7088adbbf
Amjadoudeh <Amjad.oudeh88@gmail.com>
2021-12-06 13:49:53 +0100

    Initial Commit

---
+++ StackViewExersize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackViewExersize
+//
+//  Created by Amjad Oudeh on 06.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
